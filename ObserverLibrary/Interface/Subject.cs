45e9632e60650d5ce04d3f1b80500c6c5dabec19
Даниил <kourin.daniil@gmail.com>
2022-02-24 09:53:34 +0300

    все

---
+++ ObserverLibrary/Interface/Subject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverLibrary.Interface
+{
+    public interface Subject
+    {
+        void update();
+    }
+}
